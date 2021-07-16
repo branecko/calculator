53b999203582e780df856ba86bdc43541a38c76e
Branislav Remen <remen124@gmail.com>
2021-07-16 07:07:02 +0200

    riesenie uloha 8

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ eraseAllElement.addEventListener('click',() => {
 
 // 8. do konstanty doTheMath priradte nasu funkciu, ktora zoberie priklad z displeja a vypocita ho pomocou funkcie eval()
 // dokumentacia: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval 
-// const doTheMath;
+const doTheMath = () => {
+    displayElement.textContent = eval(displayElement.textContent);
+};
 
 // 9. pristupte ku elementu = a zaistite, aby sa po kliknuti vykonal vypocet pomocou funkcie doTheMath()
 // const executeElement;
